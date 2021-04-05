017599e4188053e23545a5c1668d56107aa080c8
Juan <juandanielpadilla0@gmail.com>
2021-04-05 10:30:41 +0100

    Creación de modulo(main, months)

---
+++ financemanagement/src/app/modules/main/modules/months/components/list-detail/list-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-detail',
+  templateUrl: './list-detail.component.html',
+  styleUrls: ['./list-detail.component.scss']
+})
+export class ListDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ financemanagement/src/app/modules/main/modules/months/months.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-months',
+  templateUrl: './months.component.html',
+  styleUrls: ['./months.component.scss']
+})
+export class MonthsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
