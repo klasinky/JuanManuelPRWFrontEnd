beaea338d6c106c44755ac12d7bc9cf0d5ccece0
klasinky <manugonzl25@gmail.com>
2021-05-01 19:25:41 +0100

    Pagination en la lista de meses

--- financemanagement/src/app/modules/main/components/months/months.component.ts
+++ financemanagement/src/app/modules/main/components/months/months.component.ts
@@ -12,33 +12,85 @@ import { StorageService } from 'src/app/services/storage.service';
 })
 export class MonthsComponent implements OnInit {
 
+  totalMonths?: number;
+  nextUrl?: string;
+  previousUrl?: string;
   months!: Months[];
+  loading: boolean = false;
+  numberPagination: number[];
 
-  constructor(private httpService: HttpService, 
+  constructor(private httpService: HttpService,
     private storageService: StorageService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService) {
+    this.numberPagination = Array(10).fill(0).map((x, i) => i);
+  }
 
   ngOnInit(): void {
     this.getMonths()
   }
 
   async getMonths() {
+    this.loading = true;
     this.httpService.getAuth('months/all').subscribe(
       (data: any) => {
         console.log(data);
-        
-        this.months = data.results as Months[]; 
+        this.loading = false;
+
+        this.totalMonths = data.count;
+        this.nextUrl = data.next;
+        this.previousUrl = data.previous;
+        this.months = data.results as Months[];
+        console.log("N: " + this.nextUrl);
+        console.log("P: " + this.previousUrl);
+
       },
       (error) => {
+        this.loading = false;
+
       }
     )
   }
 
-  async deleteMonth(month: Months){
+  getStyle(){
+    return {'background':'#42141E', 
+    'border-radius': '0', 
+    'height': '85px', 
+    'margin-bottom':'0',
+    'box-shadow': '2px 20px 30px #42141E'
+  }
+  }
+
+
+  changeUrl(isNext: boolean) {
+
+    const url = isNext ? this.nextUrl : this.previousUrl;
+    if (url) {
+      this.loading = true;
+      this.httpService.getAuth(url, true).subscribe(
+        (data: any) => {
+          this.loading = false;
+
+          console.log(data);
+          this.totalMonths = data.count;
+          this.nextUrl = data.next;
+          this.previousUrl = data.previous;
+          this.months = data.results as Months[];
+        },
+        (error) => {
+          this.loading = false;
+
+        }
+      );
+    }
+
+
+  }
+
+  async deleteMonth(month: Months) {
     this.httpService.deleteAuth(month.url + '').subscribe(
       (data: any) => {
         this.months.splice(this.months.indexOf(month), 1);
-        this.toastr.success('Has eliminado el mes correctamente','Mes eliminado');
+        this.toastr.success('Has eliminado el mes correctamente', 'Mes eliminado');
       },
       (error) => {
         this.toastr.error(error.detail, "Ha ocurrido un error");
