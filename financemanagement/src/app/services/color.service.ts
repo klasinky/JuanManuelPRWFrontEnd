8cd30214320f4f2c617a4a8e00a6fcd6b545ab9e
klasinky <manugonzl25@gmail.com>
2021-05-10 21:53:52 +0100

    Avatar en los post y skeleton

---
+++ financemanagement/src/app/services/color.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorService {
+
+  constructor() { }
+
+  getColor(username: string = "") {
+    const colors = [
+      { 'background-color': '#EFD600', 'color': '#000000' },
+      { 'background-color': '#2196F3', 'color': '#ffffff' },
+      { 'background-color': '#0312AB', 'color': '#ffffff' },
+      { 'background-color': '#AB0399', 'color': '#ffffff' },
+      { 'background-color': '#AB6403', 'color': '#ffffff' }]
+
+    const random = username.length % colors.length;
+    return colors[random];
+  }
+}
