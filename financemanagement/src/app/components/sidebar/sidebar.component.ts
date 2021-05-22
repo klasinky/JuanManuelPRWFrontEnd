4c5c5e4150909a2db033d5c87da63ae02b5db23f
klasinky <manugonzl25@gmail.com>
2021-05-22 18:54:18 +0100

    WebSocket

--- financemanagement/src/app/components/sidebar/sidebar.component.ts
+++ financemanagement/src/app/components/sidebar/sidebar.component.ts
@@ -3,6 +3,10 @@ import { Notification } from 'src/app/interfaces/notification';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { HttpService } from 'src/app/services/http.service';
+import { SocketService } from 'src/app/services/socket.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sidebar',
@@ -13,7 +17,9 @@ export class SidebarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private httpService: HttpService) { }
+    private httpService: HttpService,
+    private socketNotification: SocketService,
+    private toastr: ToastrService) { }
   @Input() userData!: User;
 
 
@@ -21,15 +27,26 @@ export class SidebarComponent implements OnInit {
   notifications: any[] = [];
   ring: boolean = true;
   firstTime: boolean = true;
+  destroyed$ = new Subject();
 
   ngOnInit(): void {
     this.getNotifications();
+    const notiSub$ = this.socketNotification.connect().pipe(
+      takeUntil(this.destroyed$),
+    );
+    console.log(notiSub$);
+    notiSub$.subscribe(notification => {
+      this.notifications.unshift(notification);
+      this.activateRing();
+      this.toastr.success("Tienes una nueva notificación")
+    });
   }
 
   getNotifications() {
     this.httpService.getAuth('notifications').subscribe(
       (data) => {
         this.notifications = data as any[];
+        this.activateRing();
       },
       (error) => {
         console.log(error);
@@ -55,16 +72,15 @@ export class SidebarComponent implements OnInit {
   }
 
   activateRing() {
-    if (this.firstTime) {
-      this.firstTime = false;
-    } else {
-      this.getNotifications();
-    }
     this.ring = false
     this.ring = true;
     setTimeout(() => {
       this.ring = false;
 
     }, 1500)
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
   }
 }
